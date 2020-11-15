64b05fba1c8a241a647c618a61a8d0989ba1f40c
Pedro H <63135543+Pedro0110@users.noreply.github.com>
2020-11-15 16:56:41 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+	return (
+		<list></list>
+	);
+}
+function list(){
+	React.createElement(){
+		<div class="container">
+			<div class="col-12">
+				<div class="form-group">
+					<label for="item"><b>Escreva:</b> </label>
+					<input name="item" class="form-control" value="" />
+				</div>
+			</div>
+			<ul>
+				<li></li>
+				
+			</ul>
+			<div class="col-12">
+				<button type="button" class="btn btn-primary btn-block">Teste</button>
+			</div>
+	</div>
+	}
+}
+
+export default App;
